0d02c5890796a860590338b809ef733fbf4100c8
suchait007 <159273969+suchait007@users.noreply.github.com>
2025-05-06 18:11:17 +0530

    adding logic for calculating data

--- src/day5/ClassOP.java
+++ src/day5/ClassOP.java
@@ -1,4 +1,10 @@
 package day5;
 
 public class ClassOP {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+
+    System.out.println("Some more changes here");
+  }
 }
